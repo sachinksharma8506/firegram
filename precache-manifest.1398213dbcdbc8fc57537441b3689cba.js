7a228a9dd37367d65bf84c2b0e2d451a893d82e3
Sachin Sharma <sachin.k.sharma@globallogic.com>
2022-02-08 05:44:26 +0530

    Updates

---
+++ precache-manifest.1398213dbcdbc8fc57537441b3689cba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2452ef8635c083bef32c2619e4fa909f",
+    "url": "/firegram/index.html"
+  },
+  {
+    "revision": "003c03a02f55c39e1974",
+    "url": "/firegram/static/css/main.15b88e0d.chunk.css"
+  },
+  {
+    "revision": "456f99ad54b1f93c467e",
+    "url": "/firegram/static/js/2.882316c0.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/firegram/static/js/2.882316c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003c03a02f55c39e1974",
+    "url": "/firegram/static/js/main.9608328a.chunk.js"
+  },
+  {
+    "revision": "a20b20e63cdcd0ca9e81",
+    "url": "/firegram/static/js/runtime-main.9968160c.js"
+  }
+]);
